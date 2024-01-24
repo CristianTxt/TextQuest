948c5eef7173695efc9efe3981e0fe6c0872b93a
CristianTxt <79579732+CristianTxt@users.noreply.github.com>
2024-01-24 13:21:41 +0100

    Modified userRoute

--- routes/userRoute.mjs
+++ routes/userRoute.mjs
@@ -1,58 +1,54 @@
-import express, { response } from "express";
-import User from "../modules/user.mjs"; 
+import express from "express";
+import User from "../modules/user.mjs";
 import HttpCodes from "../modules/httpErrorCodes.mjs";
 
+const USER_API = express.Router();
 
-const USER_API = express.Router(); 
+const users = [];
+function generateRandomString(length) {
+    const characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      result += characters.charAt(randomIndex);
+    }
+    return result;
+  }
 
-const users = []; 
 
-USER_API.get('/:id',(req, res)=>{ 
+USER_API.get("/:id", (req, res) => {});
+res.status(HttpCodes.SuccesfullRespons.Ok).send(users.id).end();
 
 
+USER_API.get("/", (req, res) => {
+  res.status(HttpCodes.SuccesfullRespons.Ok).send(users).end();
 });
 
-USER_API.get('/',(req, res)=>{ 
-res.status(HttpCodes.SuccesfullRespons.Ok).send(users).end();
-});
+USER_API.post("/", (req, res, next) => {
+  const { name, email, password } = req.body;
 
-USER_API.post("/", (req, res, next)=>{ 
-
-const { name, email, password} = req.body;
-
-if ( name !="" && email !="" && password !=""){ 
-    const user = new User(); 
+  if (name !== "" && email !== "" && password !== "") {
+    const user = new User();
+    user.id = generateRandomString(7); // Change 8 to the desired length
     user.name = name;
-    user.email = email; 
+    user.email = email;
+    user.pswHash = password;
 
-   user.pswHash = password; 
+    let exists = users.some((u) => u.email === email);
 
-
-
-   let exists = false; 
-
-   if(!exists){ 
-    users.push(user);
-    res.status(HttpCodes.SuccesfullRespons.Ok).send(users).end();
-   } else { 
-    res.status(HttpCodes.ClientSideErrorRespons.BadRequest).end();
-   }
-
-
-} else { 
+    if (!exists) {
+      users.push(user);
+      res.status(HttpCodes.SuccesfullRespons.Ok).send(users).end();
+    } else {
+      res.status(HttpCodes.ClientSideErrorRespons.BadRequest).send("User with this email already exists").end();
+    }
+  } else {
     res.status(HttpCodes.ClientSideErrorRespons.BadRequest).send("Mangler data felt").end();
-}
+  }
+});
 
+USER_API.put("/:id", (req, res) => {});
 
-}); 
+USER_API.delete("/:id", (req, res) => {});
 
-
-USER_API.put('/:id', (req, res)=> {
-
-})
-
-
-USER_API.delete('/:id', (req, res)=> { 
-
-})
-export default USER_API
+export default USER_API;
