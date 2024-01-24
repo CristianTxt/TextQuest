4fb4d074107ef888b8daac3baf91c41e0a140be3
CristianTxt <79579732+CristianTxt@users.noreply.github.com>
2024-01-24 15:06:18 +0100

    added import fs that import module to read json files

--- routes/userRoute.mjs
+++ routes/userRoute.mjs
@@ -1,6 +1,7 @@
 import express from "express";
 import User from "../modules/user.mjs";
 import HttpCodes from "../modules/httpErrorCodes.mjs";
+import fs from "fs"; 
 
 const USER_API = express.Router();
 
