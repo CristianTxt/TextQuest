d9b14e081de08bd7548b7125972c8f3f8e2da3d1
CristianTxt <79579732+CristianTxt@users.noreply.github.com>
2024-02-26 14:38:27 +0100

    Fixed name problem with public folder

--- server.mjs
+++ server.mjs
@@ -15,7 +15,7 @@ const logger = new SuperLogger();
 server.use(logger.createAutoHTTPRequestLogger());
 
 server.use(express.json());
-server.use(express.static('public'));
+server.use(express.static('Public'));
 
 server.use("/user", USER_API);
 
@@ -28,7 +28,6 @@ server.get("/", (req, res, next) => {
 
 
     res.status(200).send(JSON.stringify({ msg: "This is a message..." })).end();
-    next
 });
 
 server.listen(server.get('port'), function () {
