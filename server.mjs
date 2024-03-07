cac25e5bc5c54b6404aa92f85d175cad645a5296
CristianTxt <79579732+CristianTxt@users.noreply.github.com>
2024-03-07 12:45:07 +0100

    changed the files

--- server.mjs
+++ server.mjs
@@ -9,14 +9,9 @@ printDeveloperStartupInportantInformationMSG();
 
 // Creating an instance of the server
 const server = express();
-
-
-
 // Selecting a port for the server to use.
-const port = (process.env.PORT || 5432);
+const port = (process.env.PORT || 8080);
 server.set('port', port);
-
-
 
 
 // Enable logging for server
@@ -24,19 +19,11 @@ const logger = new SuperLogger();
 server.use(logger.createAutoHTTPRequestLogger()); // Will logg all http method requests
 
 
-
-
-
 // Defining a folder that will contain static files.
 server.use(express.static('public'));
 
-
-
-
 // Telling the server to use the USER_API (all urls that uses this code will have to have the /user after the base address)
 server.use("/user", USER_API);
-
-
 
 // A get request handler example)
 server.get("/", (req, res, next) => {
